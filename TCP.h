9c7bdd457d78d626636fec1a34d5e50d9216a957
antoineludwig <antoine.ludwig60@gmail.com>
2014-06-30 20:23:19 +0200

    Premiere version

---
+++ TCP.h
@@ -0,0 +1,52 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+typedef struct {
+	
+	int src_port;
+	int dest_port;
+	int seq_num;
+	int ack;
+	int data_offset;
+	int reserved;
+	int window;
+	int checksum;
+	int urgent;
+	int option;
+	int padding;
+	int data;
+} header;
+
+int *IntToBinary(int i){
+	int a[20]; 
+	int j=0;
+      	while(i>0) 
+      	{ 
+		a[j]=i%2; 
+		j++; 
+		i=i/2;
+        }
+	return a;
+}
+
+int BinaryToInt(int n){
+	int decimal=0, i=0, rem;
+	while (n!=0)
+   	{
+        	rem = n%10;
+        	n/=10;
+        	decimal += (int)rem*pow(2,i);
+        	++i;
+    	}
+    	return decimal;
+}
---
+++ server.c
@@ -0,0 +1,102 @@
+#include "TCP.h"
+
+int main(int argc, char* argv[]){
+//test entrées
+	if(argc!=2){
+		printf("Nombre d'arguments invalide\n");
+		exit(-1);
+	}
+	/*else if(sizeof(argv[1])!=sizeof(int)){
+		perror("Le numéro de port doit etre un entier\n");
+		exit(-1);		
+	}*/
+	else{
+		printf("\nLancement du serveur\n");
+	//création de la socket
+		int socketd,idSockCli;
+		struct sockaddr_in saddr, saddrCli;
+		socklen_t saddrCliLen;
+		pid_t pid;
+		header h;
+		socketd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+		if(socketd<0){
+			printf("Erreur creation socket\n");
+			exit(-1);	
+		}
+		//remplis de zero
+		bzero(&saddr,sizeof(saddr));
+		//défini la famille
+		saddr.sin_family=AF_INET;
+		//defini le port
+		saddr.sin_port=htons(atoi(argv[1]));	
+		//associe adresse
+		saddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	//handler
+	//bind socket
+		if(bind(socketd,(const struct sockaddr *)&saddr,sizeof(saddr))<0){
+			printf("Erreur bind socket\n");
+			exit(-1);	
+		}
+	//listen socket
+		if(listen(socketd,SOMAXCONN)<0){
+			printf("Erreur ecoute socket\n");
+			exit(-1);	
+		}
+	//lecture socket	
+		while(1){
+		
+			//recup addr client & acceptation connection client socket
+			saddrCliLen=sizeof(saddrCli);
+			idSockCli=accept(socketd,(struct sockaddr *)&saddrCli,&saddrCliLen);
+			if(idSockCli<0){
+				printf("Erreur creation socket\n");
+				exit(-1);	
+			}
+		
+			//affichage test client connecte		
+			int port;
+			char adresseIP[16];
+			port=ntohs(saddrCli.sin_port); 
+			inet_ntop(AF_INET,&saddrCli.sin_addr,adresseIP,sizeof(adresseIP)); 
+			printf("connexion de %s, port %d\n",adresseIP,port);
+		//test reception
+			pid=fork();
+			int b=0;
+			int rec=0;
+			if(pid==0){
+				rec=recv(idSockCli,&b,sizeof(int),0);
+				if(rec<0){
+					printf("err=%d\n",rec);
+				}
+				else
+					printf("b=%d\n",b);
+				printf("%s\n",strerror(errno));
+			}
+			else if(pid==-1){
+				exit(-1);
+			}
+			else{
+				printf("Pere\n");
+			}
+		//fin du test
+
+//envoi de la structure complète (pas champ par champ)
+		int taille_envoi = sizeof(header);
+
+		printf("Envoi d'un message de taille %d\n",taille_envoi);
+
+		printf("Envoi de la taille du message au serveur\n");
+		send(idSockCli,&taille_envoi,taille_envoi,0);
+
+		printf("Envoi du message\n");
+		//envoi sur la socket de la structure, avec un pointeur sur la struct définie et la taille de la structure (obtenue par 		le sizeof ligne 112)
+		if(send(idSockCli,&header,taille_envoi,0)==-1)
+		{
+			perror("send()");
+			exit(EXIT_FAILURE);
+		}
+		
+		}
+	}
+	return 0;
+}
